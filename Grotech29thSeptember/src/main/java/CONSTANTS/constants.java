0a78675ea064ea4b933c6242f918a1c680b9296c
Automation_TestingProject <masagi16@gmail.com>
2023-10-25 18:21:46 +0530

    commiting the project

---
+++ Grotech29thSeptember/src/main/java/CONSTANTS/constants.java
@@ -0,0 +1,9 @@
+package CONSTANTS;
+
+public class constants {
+	
+	public static final String URLPATH="C:\\Users\\Manju\\Desktop\\XLTestData\\AppURL.xlsx";
+	public static final String SCREENSHOTPATH="‪D:\\TestData\\ScreenShots"+Math.random()+"test.png";
+	
+	
+}
