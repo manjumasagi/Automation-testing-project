1be71eb0b112a26daa81930d10cbd0bc1b6fa849
Automation_TestingProject <masagi16@gmail.com>
2023-11-02 08:04:41 +0530

    commmiting mult prog

---
+++ multiplication_demo.java
@@ -0,0 +1,18 @@
+package Batch2GroTech;
+
+public class multiplication_demo {
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Multiplication process will get started");
+		System.out.println("Enter values of a and b and result will be stored in c");
+		
+		int a=10,b=15;
+		int c=a*b;
+		
+		System.out.println("when value of a is "+a +"and value of b is "+b + "then value of c is "+c);
+
+	}
+
+}
